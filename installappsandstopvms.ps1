c7ca3ef1f3722d0b49cb2361d844d2f3beab612e
Broer Allard <allardv03@gmail.com>
2024-11-30 14:11:00 +0100

    Update and rename stop-vms.ps1 to installappsandstopvms.ps1

--- installappsandstopvms.ps1
+++ installappsandstopvms.ps1
@@ -1,3 +1,11 @@
+# Install App Installer (includes winget)
+Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=2108834 -OutFile AppInstaller.msixbundle
+Add-AppxPackage -Path .\AppInstaller.msixbundle
+
+# Install Office and 7zip silently
+winget install --id Microsoft.Office -e --silent
+winget install --id 7zip.7zip -e --silent
+
 # Define the resource group and VM names
 $RESOURCE_GROUP = "Harderwijk-1"
 $VM_NAMES = @("werkplekvm1", "werkplekvm2", "werkplekvm3", "werkplekvm4", "werkplekvm5")
