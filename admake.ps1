123108081cee15d2f916f7897e2ca3deaec7a449
Rudy Houkema <rudylee-houkema@hotmail.com>
2024-12-06 13:37:58 +0100

    admake change

--- admake.ps1
+++ admake.ps1
@@ -8,17 +8,33 @@ param (
     [SecureString]$SafeModeAdministratorPassword
 )
 
-# Installeren van AD DS rol
-Write-Host "Installeren van Active Directory Domain Services (AD DS)..."
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+# Controleer of de AD DS-rol al is geïnstalleerd
+Write-Host "Controleren of Active Directory Domain Services (AD DS) al is geïnstalleerd..."
+if (-not (Get-WindowsFeature -Name AD-Domain-Services).Installed) {
+    Write-Host "Installeren van AD DS-rol..."
+    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -ErrorAction Stop
+    Write-Host "AD DS-rol succesvol geïnstalleerd."
+} else {
+    Write-Host "AD DS-rol is al geïnstalleerd."
+}
 
 # Configureren van het domein
-Write-Host "Configureren van een nieuw Active Directory Forest op Windows Server 2022..."
-Install-ADDSForest `
-    -DomainName $DomainName `
-    -DomainNetbiosName $NetbiosName `
-    -ForestMode Win2022 `
-    -SafeModeAdministratorPassword $SafeModeAdministratorPassword `
-    -Force
+Write-Host "Controleren of een Active Directory-domein al aanwezig is..."
+if (-not (Test-Path "$Env:SystemRoot\NTDS")) {
+    try {
+        Write-Host "Configureren van een nieuw Active Directory Forest op Windows Server 2022..."
+        Install-ADDSForest `
+            -DomainName $DomainName `
+            -DomainNetbiosName $NetbiosName `
+            -ForestMode Win2022 `
+            -SafeModeAdministratorPassword $SafeModeAdministratorPassword `
+            -Force -ErrorAction Stop
 
-Write-Host "Active Directory-configuratie voltooid. De server zal automatisch opnieuw opstarten."
+        Write-Host "Active Directory-configuratie voltooid. De server zal automatisch opnieuw opstarten."
+    } catch {
+        Write-Error "Er is een fout opgetreden tijdens de configuratie: $_"
+        exit 1
+    }
+} else {
+    Write-Host "Er bestaat al een Active Directory-domein op deze server."
+}
