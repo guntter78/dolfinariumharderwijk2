1f00fe65632a4321aab0ca2095f664dedcc38eac
Broer Allard <allardv03@gmail.com>
2024-12-09 18:30:23 +0100

    Update werkpleksetup.ps1

--- werkpleksetup.ps1
+++ werkpleksetup.ps1
@@ -1,15 +1,15 @@
-param (
-  [Parameter(Mandatory=$false)]
-  [string]$IpAddress,
-  [Parameter(Mandatory=$false)]
-  [string]$Username,
-  [Parameter(Mandatory=$true)]
-  [string]$DomainName,
-  [Parameter(Mandatory=$true)]
-  [string]$Password
-)
+# param (
+#   [Parameter(Mandatory=$false)]
+#   [string]$IpAddress,
+#   [Parameter(Mandatory=$false)]
+#   [string]$Username,
+#   [Parameter(Mandatory=$true)]
+#   [string]$DomainName,
+#   [Parameter(Mandatory=$true)]
+#   [string]$Password
+# )
 
-Set-TimeZone -Id "W. Europe Standard Time"  
+# Set-TimeZone -Id "W. Europe Standard Time"  
 
 powershell -ExecutionPolicy bypass -File 7ZipSetup.ps1
-powershell -ExecutionPolicy bypass -File joinAD2.ps1 -IpAddress $IpAddress -Username $Username -DomainName $DomainName -Password $Password
+# powershell -ExecutionPolicy bypass -File joinAD2.ps1 -IpAddress $IpAddress -Username $Username -DomainName $DomainName -Password $Password
