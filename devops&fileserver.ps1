f5058c4e3ba17c2c5b5253d31a533020c06c4872
Broer Allard <allardv03@gmail.com>
2024-12-11 10:54:52 +0100

    Update and rename devops&fileserver.ps1 to devops.ps1

--- devops&fileserver.ps1
+++
@@ -1,2 +0,0 @@
-powershell -ExecutionPolicy bypass -File DevOps.ps1
-powershell -ExecutionPolicy bypass -File fileserver.ps1
---
+++ devops.ps1
@@ -0,0 +1,25 @@
+param (
+  [Parameter(Mandatory=$false)]
+  [string]$IpAddress,
+  [Parameter(Mandatory=$false)]
+  [string]$Username,
+  [Parameter(Mandatory=$true)]
+  [string]$DomainName,
+  [Parameter(Mandatory=$true)]
+  [string]$Password
+)
+
+Set-TimeZone -Id "W. Europe Standard Time"  
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/guntter78/dolfinariumharderwijk2/main/mul_azure_devops_server_express_2022.2_x64_web_installer_4db8d6ad.exe -OutFile C:\AzureDevOpsServer2022.2.exe;
+Start-Process -FilePath 'C:\AzureDevOpsServer2022.2.exe' -ArgumentList '/quiet' -Wait;
+
+$interfaces = Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.ServerAddresses -ne $null }
+Set-DnsClientServerAddress -InterfaceAlias $interfaces.InterfaceAlias -ServerAddresses ($IpAddress,"10.10.2.6")
+
+$DomainUser = $Username + '@' + $DomainName
+
+$Cred = New-Object System.Management.Automation.PSCredential ($DomainUser, (ConvertTo-SecureString $Password -AsPlainText -Force))
+Add-Computer -DomainName $DomainName -Credential $Cred
+
+Restart-Computer -Force -Wait
