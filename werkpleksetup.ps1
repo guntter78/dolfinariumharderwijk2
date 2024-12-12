4ddebb826b0708f7e524d8fe3b520d08661d5424
Broer Allard <allardv03@gmail.com>
2024-12-12 19:18:57 +0100

    Update werkpleksetup.ps1

--- werkpleksetup.ps1
+++ werkpleksetup.ps1
@@ -13,7 +13,7 @@ Set-TimeZone -Id "W. Europe Standard Time"
 
 Invoke-WebRequest -Uri "https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=O365HomePremRetail&platform=X86&language=en-US&TaxRegion=pr&correlationId=7e21dd05-491e-4160-9092-2def0c1f1548&token=03ec93e1-7670-4b54-ae66-c70b47d595b2&version=O15GA&source=O15OLSOMEX" -OutFile "$env:TEMP\Setup.exe"
 cd $env:TEMP
-.\Setup.exe
+.\Setup.exe --provision true --quiet --start
 
 
 $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object {($_.outerHTML -match 'Download')-and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} | Select-Object -First 1 | Select-Object -ExpandProperty href)
