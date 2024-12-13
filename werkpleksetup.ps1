a6ab6c9c81504d3930d147fa3ce57eb71789fd76
Broer Allard <allardv03@gmail.com>
2024-12-13 14:26:53 +0100

    Update werkpleksetup.ps1

--- werkpleksetup.ps1
+++ werkpleksetup.ps1
@@ -11,9 +11,14 @@ param (
 
 Set-TimeZone -Id "W. Europe Standard Time"  
 
-Invoke-WebRequest -Uri "https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=O365HomePremRetail&platform=X86&language=en-US&TaxRegion=pr&correlationId=7e21dd05-491e-4160-9092-2def0c1f1548&token=03ec93e1-7670-4b54-ae66-c70b47d595b2&version=O15GA&source=O15OLSOMEX" -OutFile "$env:TEMP\Setup.exe"
+Invoke-WebRequest -Uri "https://github.com/guntter78/dolfinariumharderwijk2/raw/refs/heads/main/setup.exe" -OutFile "$env:TEMP\setup.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/guntter78/dolfinariumharderwijk2/refs/heads/main/Configuratie.xml" -OutFile "$env:TEMP\Configuratie.xml"
+
 cd $env:TEMP
-.\Setup.exe --provision true --quiet --start
+
+.\setup.exe /download .\Configuration.xml
+.\setup.exe /configure .\Configuration.xml
+
 
 
 $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object {($_.outerHTML -match 'Download')-and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} | Select-Object -First 1 | Select-Object -ExpandProperty href)
