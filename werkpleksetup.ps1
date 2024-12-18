6f2e0dbc9d29a9e36b335af840652b578ca40e8f
Broer Allard <allardv03@gmail.com>
2024-12-18 13:16:42 +0100

    Update werkpleksetup.ps1

--- werkpleksetup.ps1
+++ werkpleksetup.ps1
@@ -9,10 +9,6 @@ param (
   [string]$Password
 )
 
-Set-TimeZone -Id "W. Europe Standard Time"  
-
-
-
 $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object {($_.outerHTML -match 'Download')-and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} | Select-Object -First 1 | Select-Object -ExpandProperty href)
 # modified to work without IE
 # above code from: https://perplexity.nl/windows-powershell/installing-or-updating-7-zip-using-powershell/
@@ -21,6 +17,7 @@ Invoke-WebRequest $dlurl -OutFile $installerPath
 Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
 Remove-Item $installerPath
 
+# Replace the parameters with your own values
 
 $interfaces = Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.ServerAddresses -ne $null }
 Set-DnsClientServerAddress -InterfaceAlias $interfaces.InterfaceAlias -ServerAddresses ($IpAddress,"10.10.2.6")
@@ -29,6 +26,5 @@ $DomainUser = $Username + '@' + $DomainName
 
 $Cred = New-Object System.Management.Automation.PSCredential ($DomainUser, (ConvertTo-SecureString $Password -AsPlainText -Force))
 Add-Computer -DomainName $DomainName -Credential $Cred
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Everyone"
 
-Restart-Computer -Force
+Restart-Computer -Force -Wait
